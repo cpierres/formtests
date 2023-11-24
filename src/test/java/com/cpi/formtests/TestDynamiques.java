a7fd7ac55adcbb97a9e03a0fcd532b69ab944487
cpierres <cpierres@hotmail.com>
2023-11-24 18:02:11 +0100

    Tests dynamiques (@TestFactory) La fabrique peut aussi renvoyer un Stream de DynamicTest

--- src/test/java/com/cpi/formtests/TestDynamiques.java
+++ src/test/java/com/cpi/formtests/TestDynamiques.java
@@ -7,13 +7,23 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Les tests dynamiques sont des tests qui sont générés à l'exécution par une méthode de type fabrique qui
  * est annotée avec @TestFactory. Les méthodes annotées avec @TestFactory ne sont donc pas des cas de tests
- * mais des fabriques pour fournir un ensemble de cas de tests.
+ * mais des fabriques pour fournir un ensemble de cas de tests.<br>
+ * Le moteur d'exécution de JUnit va invoquer les méthodes annotées avec @TestFactory, ajouter le DynamicTest
+ * obtenu dans les tests et l'exécuter.<br>
+ * Les méthodes annotées avec @TestFactory peuvent avoir si besoin des paramètres dont les valeurs devront être
+ * injectées grâce à des ParameterResolvers.<br>
+ * L'exécution des DynamicTest est différente de celle des tests standards : les cas de tests dynamiques ne peuvent
+ * pas avoir de méthodes du cycle de vie invoquées. Les méthodes annotées avec @BeforeEach et @AfterEach sont
+ * invoquées pour la méthode annotée avec @TestFactory mais elles ne sont pas invoquées lors de l'exécution des cas
+ * de tests créés dynamiquement.
  */
 public class TestDynamiques {
 
@@ -47,6 +57,7 @@ public class TestDynamiques {
 
     /**
      * La fabrique peut aussi renvoyer un Iterator de DynamicTest
+     *
      * @return
      */
     @TestFactory
@@ -60,5 +71,18 @@ public class TestDynamiques {
         return resultat.iterator();
     }
 
+    /**
+     * La fabrique peut enfin renvoyer un Stream de DynamicTest.
+     * Si la fabrique renvoie un Stream, sa méthode close() sera invoquée à la fin de l'exécution des tests dynamiques
+     * qu'il contient, ce qui est important si la source du Stream requiert sa fermeture.
+     *
+     * @return
+     */
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsAvecStream() {
+        return IntStream.rangeClosed(1, 5)
+                .mapToObj(val -> DynamicTest.dynamicTest("Ajout " + val + "+" + val,
+                        () -> assertEquals(val * 2, val + val)));
+    }
 
 }
